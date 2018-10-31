075e92e4d6eb39ec11dcc3f4eef0c5a66b63f7d2
shinate <shine.wangrs@gmail.com>
2018-10-31 16:18:48 +0800

    update

---
+++ clear.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const map = require('map-stream');
+const vfs = require('vinyl-fs');
+const jsonfile = require('jsonfile');
+const colors = require('colors');
+const fs = require('fs');
+
+let db = {};
+try {
+    db = jsonfile.readFileSync('./db.json');
+} catch (e) {
+    // no file
+}
+
+let files = [];
+for (let i in db) {
+    files = files.concat(db[i]);
+}
+
+if (files.length) {
+    vfs.src(['./**/*.{jpg,png,gif}', '!./node_modules/**/*', '!./source/**/*'])
+        .pipe(map((file, cb) => {
+            if (0 > files.indexOf(file.basename)) {
+                // 不村在
+                try {
+                    fs.unlinkSync(file.path);
+                    console.log('[File deleted]'.bold.red, file.path);
+                } catch (e) {
+                    console.log('[File delete failed]'.bold.yellow, file.path);
+                }
+            }
+        }));
+}
